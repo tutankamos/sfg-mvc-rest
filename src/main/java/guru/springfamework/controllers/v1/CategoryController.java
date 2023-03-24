7f742e9f648896fc7de80fad3413050758c8d615
Amos Torretta <torrettaamos@gmail.com>
2023-03-24 16:15:47 +0100

    refactor da controller a RESTCONTROLLER, con i response status e un codice piu' pulito

--- src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -6,11 +6,9 @@ import guru.springfamework.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController // combina @controller con @Responsebody
 @RequestMapping("/api/v1/categories/")
 public class CategoryController {
 
@@ -21,20 +19,15 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CatorgoryListDTO> getallCatetories() {
+    @ResponseStatus(HttpStatus.OK)
+    public CatorgoryListDTO getallCatetories() {
+        return new CatorgoryListDTO(categoryService.getAllCategories());
 
-        return new ResponseEntity<CatorgoryListDTO>(
-                new CatorgoryListDTO(
-                        categoryService.getAllCategories()),
-                HttpStatus.OK
-        );
     }
 
     @GetMapping("{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-        return new ResponseEntity<CategoryDTO>(
-                categoryService.getCategoryByName(name),
-                HttpStatus.OK
-        );
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
